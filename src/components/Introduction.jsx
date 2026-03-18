889d8bc59f239ae383bdc465ce2788406257407b
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-18 16:51:14 +1100

    feat: rename Navbar to Introduction component

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+function Introduction() {
+    return (
+        <section>
+            <h1 className="text-black">
+                Hi, I am <span className="border-b border-current">William Hok</span>
+            </h1>
+            <div className="">
+                <p>
+                    A passionate RMIT Computer Science student from Indonesia.
+                    I am interested to build scalable applications, learning 
+                    technologies, and contributing to projects that solve real
+                    world problems
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Introduction
