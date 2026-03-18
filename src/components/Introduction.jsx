f703207ccefab196de9905ab9749e9900ac41239
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-18 21:01:24 +1100

    feat: add GitHub and LinkedIn links

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -11,7 +11,10 @@ function Introduction() {
                     A passionate RMIT Computer Science student from Indonesia.
                     I am interested to build scalable applications, learning 
                     technologies, and contributing to projects that solve real
-                    world problems
+                    world problems.
+                </p>
+                <p className="pt-4">
+                    Find me on <a href="https://github.com/wonganjatan" target="_blank" className="text-blue-500 hover:border-b hover:border-blue-500 transition-all duration-300 ease-in-out">GitHub</a> and <a href="https://www.linkedin.com/in/william-hok-078aa63b7/" target="blank" className="text-blue-500 hover:border-b hover:border-blue-500 transition-all duration-300 ease-in-out">LinkedIn</a>
                 </p>
             </div>
         </section>
