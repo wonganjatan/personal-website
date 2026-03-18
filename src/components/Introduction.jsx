8724c28f4b7b3b183a34abbaf86ab70e534bcb94
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-03-18 21:06:11 +1100

    style: contain the introduction to card

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -3,10 +3,10 @@ import { useState } from "react"
 function Introduction() {
     return (
         <section>
-            <h1>
-                Hi, I am <span className="border-b border-current">William Hok</span>
-            </h1>
-            <div className="">
+            <div className="bg-white border border-slate-200 p-8 rounded-2xl shadow-2xl">
+                <h1>
+                    Hi, I am <span className="border-b border-current">William Hok</span>
+                </h1>
                 <p>
                     A passionate RMIT Computer Science student from Indonesia.
                     I am interested to build scalable applications, learning 
@@ -17,6 +17,7 @@ function Introduction() {
                     Find me on <a href="https://github.com/wonganjatan" target="_blank" className="text-blue-500 hover:border-b hover:border-blue-500 transition-all duration-300 ease-in-out">GitHub</a> and <a href="https://www.linkedin.com/in/william-hok-078aa63b7/" target="blank" className="text-blue-500 hover:border-b hover:border-blue-500 transition-all duration-300 ease-in-out">LinkedIn</a>
                 </p>
             </div>
+            
         </section>
     )
 }
