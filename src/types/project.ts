92f4e765f0106ebedb5f62633450e50c75618792
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-08-24 14:35:59 +1000

    feat: add Project types

---
+++ src/types/project.ts
@@ -0,0 +1,9 @@
+export type Project = {
+    title: string
+    description: string
+    longDescription: string
+    techStack: string[]
+    repoUrl: string
+    liveUrl?: string
+    iamgeUrl?: string
+}
