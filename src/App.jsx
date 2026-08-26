5c44808163d28ad77236aa513a48c1581cfcec9b
wonganjatan <222377129+wonganjatan@users.noreply.github.com>
2026-08-26 15:35:26 +1000

    fix: replace the depreciated bg-gradient-* to bg-linear-*

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <html>
-      <body className='h-full font-mono flex justify-center px-20 bg-gradient-to-b from-slate-300 to-white'>
+      <body className='h-full font-mono flex justify-center px-20 bg-linear-to-b from-slate-300 to-white'>
         <main className='min-h-screen flex justify-center items-start px-6 py-10'>
           <div className='mx-auto pt-6 pb-12 max-w-xl space-y-12'>
             <Introduction/>
